74905184c7b3def8076165c145a9414d6af42e3d
Meet Jain <lodhameetr@gmail.com>
2024-04-28 13:52:43 +0530

    first commit

---
+++ project.py
@@ -0,0 +1,188 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import smtplib
+import pyaudio
+import os
+from tkinter import *
+from tkinter import ttk
+from ttkthemes import themed_tk as tk
+from translate import Translator
+import pyjokes
+import pyautogui
+
+engine = pyttsx3.init('sapi5')
+
+
+T=Translator(
+from_lang = "English", to_lang = "Hindi"
+)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def screenshot():
+    pic=pyautogui.screenshot()
+    pic.save('c:/User')
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour >= 0 and hour < 6:
+        speak("You should be sleeping")
+
+    elif hour >= 6 and hour < 12:
+        speak("Good Morning")
+
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon")
+
+    else:
+        speak("Hello sir")
+
+    speak("I'm Cole your assistant")
+    speak("Please tell me how may i help you ?")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognising.....")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print(e)
+        print("Say that again please.....")
+        return "None"
+    return query
+
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('lodhameetr@gmail.com', 'meetlodha04')
+    server.sendmail('lodhameetr@gmail.com', to, content)
+    server.close()
+
+def joke():
+    speak(pyjokes.get_jokes(language='en',category='all'))
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia....')
+            query = query.replace('wikipedia', '')
+            results = wikipedia.summary(query, sentences=2)
+            speak('According to wikipedia')
+            speak(results)
+            print(results)
+
+
+        elif 'wish me' in query:
+            wishMe()
+
+        elif 'open college website' in query:
+            webbrowser.open("https://djsce.ac.in/")
+
+        elif 'start youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'play song' in query:
+            speak("what do you wanna hear ?")
+            search= takeCommand().lower()
+            speak('There you go...')
+            webbrowser.open('https://www.youtube.com/results?search_query='+search)
+
+        elif 'open LinkedIn' in query:
+            webbrowser.open("www.linkedin.com/in/meet-jain-a5a103249/")
+
+        elif 'open instagram' in query:
+            webbrowser.open("instagram.com")
+
+        elif 'open internship' in query:
+            webbrowser.open("https://djsceinternshipfair.netlify.app/")
+
+        elif 'open whatsapp' in query:
+            webbrowser.open("https://web.whatsapp.com/")
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H,%M,%S")
+            speak(f"Sir, the time is {strTime}")
+
+
+
+        elif 'email to meet' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "lodhameetr@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry sir. I am not able to send this email")
+
+        elif 'shutdown' in query:
+            print("shutting down...")
+            speak("shutting down")
+            quit()
+
+        elif 'translate' in query:
+            speak("what do you wanna translate ?")
+            query = query.replace('translate', '')
+            translation = T.translate(query)
+            speak(translation)
+            print(translation)
+            speak(translation)
+
+        elif 'joke' in query:
+            joke()
+
+        elif 'screenshot' in query:
+            screenshot()
+
+        elif 'search youtube' in query:
+            speak("what do you wanna see ?")
+            search= takeCommand().lower()
+            speak('There you go...')
+            webbrowser.open('https://www.youtube.com/results?search_query='+search)
+            
+
+        elif 'search google' in query:
+            speak("Tell me what to search ?")
+            search=takeCommand().lower()
+            speak('There you go !')
+            webbrowser.open('https:/www.google.co.in/search?q='+search)
+
+
+
+
+def main():
+
+    root_1 = tk.ThemedTk()
+    root_1.set_theme('radiance')
+    root_1.title("Voice assistant")
+    root_1.geometry("400x350")
+    lbl1=ttk.Label(master=root_1, text="Welcome to voice assistant app\n\n", wraplength=600)
+    lbl1.pack()
+    but1 = ttk.Button(root_1, text="Run the assistant🤖🔊", command=main)
+    but1.config(width=22)
+    but1.pack(padx=10, pady=10)
+    quit4 = ttk.Button(root_1, text="EXIT", command=root_1.destroy)
+    quit4.config(width=22)
+    quit4.pack(padx=10, pady=20)
+    root_1.mainloop()
